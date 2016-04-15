75bb3257a9c7d5eb440ec06ab5de1b3d8ef23414
sonicfloatzel <sonicfloatzel@gmail.com>
2016-04-15 13:06:44 -0500

    Added files via upload

---
+++ movementtest.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+namespace fbmovementtest
+{
+    public class EasyMove : MonoBehaviour
+    {
+        public float Speed = 0f;
+        private float movex = 0f;
+        private float movey = 0f;
+        //use for initialization
+        void start()
+        {
+        }
+        //Update is called once per frame
+        void FixedUpdate()
+        {
+            movex = input.GetAxis("Horizontal");
+            movey = input.GetAxis("Vertical");
+            rigidbody2D.velocity = new Vector2(movex * Speed, movey * Speed);
+        }
+    }
+}
